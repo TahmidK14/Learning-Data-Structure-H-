ea1593557f56ef24248f03a4946443e95699ecb3
Tahmid <117463366+aUtistiC-duMPsterbaby@users.noreply.github.com>
2023-07-03 23:28:57 +0600

    Add files via upload

---
+++ day19.cpp
@@ -0,0 +1,31 @@
+//reversing a sentence using stacks
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+void reverseS(string s)
+{
+    stack<string> st;
+    for(int i=0;i<s.length();i++)
+    {
+        string word="";
+        while(s[i]!=' ' && i<s.length())
+        {
+            word+=s[i];
+            i++;
+        }
+        st.push(word);
+    }
+    while(!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+    }cout<<endl;
+}
+
+int main()
+{
+    string s="sad you are Why";
+    reverseS(s);
+}
