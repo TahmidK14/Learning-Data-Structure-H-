826232086f233ea17bed09f12636aa10b0735ea1
Tahmid <117463366+TahmidK14@users.noreply.github.com>
2023-07-16 20:41:14 +0600

    Update day6.cpp

--- day6.cpp
+++ day6.cpp
@@ -1,4 +1,4 @@
-//Initialize an array. Size should be more than FIVE. Write you program to change the array in such a way so that there cannot be any duplicate element in the array anymore. Print the changed array. If the initialized array already had no duplicate elements from the beginning, output a message saying “Array already unique!”;
+//Initialize an array. Size should be more than FIVE. Write you program to change the array in such a way so that there cannot be any duplicate element in the array anymore. Print the changed array. If the initialized array already had no duplicate elements from the beginning, output a message saying â€œArray already unique!â€;
 #include<iostream>
 using namespace std;
 int main()
@@ -10,28 +10,31 @@ int main()
 
     if(n>5)
     {
-        int arr[n];
-        int current[n];
-        for(i=0;i<n;i++)
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        int current=arr[i];
+        for(int j=i+1;j<n;j++)
         {
-            cin>>arr[i];
-        }
-        for(int i=0;i<n-1;i++)
-        {
-            current[i]=arr[i];
-            for(int j=1;j<n;j++)
+            if(current==arr[j])
             {
-                int comp[j];
-                if(current[i]=comp[j])
-                {
-                 arr[i] = arr[i + 1];
-
+                for (int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
                 }
+                n--;
+                j--;
             }
+        }
 
-            cout<<arr[i]<<" ";
-        }cout<<endl;
-
+    }
+        for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }        
     }
     else
 
